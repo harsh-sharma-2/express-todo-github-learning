a40d5a20348f49a885f95ecf8297d853718e7afe
harsh sharma <harsh.sharma1@cubexo.io>
2024-01-08 15:57:11 +0530

    github workflows

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -2,7 +2,10 @@ const request = require("supertest")
 const app = require("../index")
 
 describe("get all todos", () => {
-    it("should return all todos", () => {
-        const todos = request(app).get("/todo")
+    it("should return all todos", async () => {
+        const todos = await request(app).get("/todo");
+        expect(todos.statusCode).toBe(200);
+        console.log(todos.body.data)
+        expect(JSON.stringify(todos.body)).toBe(JSON.stringify({"data": []}))
     })
 })
