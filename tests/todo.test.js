d02d0b44b17fb83d0385143ec8e005a3d87cae40
harsh sharma <harsh.sharma1@cubexo.io>
2024-01-08 15:00:39 +0530

    todo test created

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -1,4 +1,8 @@
 const request = require("supertest")
 const app = require("../index")
 
-describe("get all todos")
+describe("get all todos", () => {
+    it("should return all todos", () => {
+        const todos = request(app).get("/todo")
+    })
+})
